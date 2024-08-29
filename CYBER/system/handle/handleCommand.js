2203a051e4f3f0dea721499149fe41558d5e6aba
MAHABUB720 <mahabub678919@gmail.com>
2024-08-29 09:56:39 +0600

    Update handleCommand.js

--- CYBER/system/handle/handleCommand.js
+++ CYBER/system/handle/handleCommand.js
@@ -1,7 +1,7 @@
 module.exports = function({ api, models, Users, Threads, Currencies }) {
   const stringSimilarity = require('string-similarity'),
     escapeRegex = (str) => str.replace(/[.*+?^${}()|[\]\\]/g, "\\$&"),
-    logger = require("../../catalogs/cyberc.js");
+    logger = require("../../catalogs/Mahabubc.js");
   const axios = require('axios')
   const moment = require("moment-timezone");
   return async function({ event }) {
