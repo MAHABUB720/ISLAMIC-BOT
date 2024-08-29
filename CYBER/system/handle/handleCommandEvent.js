071ff8cc967cc03e32f06d34b3c214731e2b1112
MAHABUB720 <mahabub678919@gmail.com>
2024-08-29 09:58:34 +0600

    Update handleCommandEvent.js

--- CYBER/system/handle/handleCommandEvent.js
+++ CYBER/system/handle/handleCommandEvent.js
@@ -1,5 +1,5 @@
 module.exports = function ({ api, models, Users, Threads, Currencies }) {
-    const logger = require("../../catalogs/cyberc.js")
+    const logger = require("../../catalogs/Mahabubc.js")
     return function ({ event }) {
         const { allowInbox } = global.ryuko;
         const { userBanned, threadBanned } = global.data;
